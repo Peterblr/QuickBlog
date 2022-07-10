a9058ad4278ae3ff7bfa791adb3ee4d489e76b94
Petr Sviryd <Peter.blr@yandex.ru>
2022-07-10 10:00:21 +0300

    Add project files.

---
+++ QuickBlog/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QuickBlog.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ QuickBlog/ViewComponents/HeaderViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuickBlog.ViewComponents
+{
+    public class HeaderViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return await Task.Factory.StartNew(() =>
+            {
+                return View();
+            });
+        }
+    }
+}
